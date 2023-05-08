256601b3178bfdbe04fa453456c78011cacecbc1
Eduardo Acuña <eduardo@tappin.no>
2023-05-08 19:04:25 +0100

    Create default index page. Install dotenv for environment variables. Starting with env file

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
+require('dotenv').config();
+
+// console.log( process.env );
 
 // Create express server
 const app = express();
 
+// Public directory
+app.use( express.static('public') );
+
 // Routes
-app.get('/', (req, res) => {
-	res.json({
-		ok: true
-	});
-});
+// app.get('/', (req, res) => {
+// 	res.json({
+// 		ok: true
+// 	});
+// });
 
 // Listen requests
-app.listen( 4000, () => {
-	console.log(`Server is running on port ${ 4000 }`);
+app.listen( process.env.PORT, () => {
+	console.log(`Server is running on port ${ process.env.PORT }`);
 } )
