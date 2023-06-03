0e4fc36cfb19fe15ee87edfbffa162dbf02d999c
Eduardo Acuña <eduardo@tappin.no>
2023-06-03 21:00:57 +0100

    Create user model. Create user into the database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,25 @@
 const { response } = require('express');
+const User = require('../models/User');
 
-const createUser = (req, res = response) => {
+const createUser = async (req, res = response) => {
 
-	const { name, email, password } = req.body;
+	// const { name, email, password } = req.body;
+
+	try {
+		const user = new User( req.body );
 	
-	res.status(201).json({
-		ok: true,
-		msg: 'reg',
-		name,
-		email,
-		password,
-	});
+		await user.save();
+		
+		res.status(201).json({
+			ok: true,
+			msg: 'reg',
+		});
+	} catch (error) {
+		res.status(500).json({
+			ok: false,
+			msg: 'Contact to Admin'
+		});
+	}
 };
 
 const loginUser = (req, res = response) => {
